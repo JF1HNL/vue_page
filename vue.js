319a25091039ba2a87d711e3857fb8aaf1f687f8
JF1HNL <1118040@ed.tus.ac.jp>
2018-08-28 12:04:26 +0900

    udonlist

--- vue.js
+++ vue.js
@@ -1,7 +1,16 @@
 let Field = new Vue({
     el: '#udon',
+    computed:{
+      udonlist : function(){
+          return ['かけうどん','ぶっかけうどん']
+      }
+    },
     data: {
         page : 1 ,
-        udon_button : 0
+        money : null ,
+        coupon : null ,
+        selectudon : null,
+        moneylist : [200,300,400,500,600],
+        couponlist : ['天ぷら50円引き','温玉無料','うどん50円引き']
     }
 })
